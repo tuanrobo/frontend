ef4e8041b648be1a2b211f9100e1ca0110e852bd
tuanrobo <tuanrobo@gmail.com>
2019-12-13 11:43:32 +0700

    Updates

---
+++ precache-manifest.8c385395bfe36c20041c3622ff6ff17d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25136f01c8d411a339ae8fb7f8cf357b",
+    "url": "/quanvanbilling/index.html"
+  },
+  {
+    "revision": "4c93d225e383abd0383d",
+    "url": "/quanvanbilling/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "3505881d765aa3ba49b4",
+    "url": "/quanvanbilling/static/css/main.e98a9dd9.chunk.css"
+  },
+  {
+    "revision": "4c93d225e383abd0383d",
+    "url": "/quanvanbilling/static/js/2.bdafd163.chunk.js"
+  },
+  {
+    "revision": "3505881d765aa3ba49b4",
+    "url": "/quanvanbilling/static/js/main.87566391.chunk.js"
+  },
+  {
+    "revision": "14f18d8d831885d2716b",
+    "url": "/quanvanbilling/static/js/runtime-main.2a7876f2.js"
+  }
+]);
